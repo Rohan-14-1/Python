4d2f36b9a4bf4144eb6f85f45b6d801fb887d9e1
Rohan-14-1 <rohanmdl2003@gmail.com>
2026-01-23 19:03:09 +0545

    Print new line

---
+++ print.py
@@ -0,0 +1,4 @@
+#print multiple line in python, we are using triple quotes
+print("""Hello, World!
+This is a multi-line string in Python.
+It can span multiple lines without needing escape characters.""")
