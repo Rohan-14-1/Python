ca3955c5393bdc7b7eb7a2359137b257b6a6dbd3
Rohan-14-1 <rohanmdl2003@gmail.com>
2026-01-24 19:46:29 +0545

    problem-1

---
+++ Problem-1.py
@@ -0,0 +1,6 @@
+#write a program to display person name, age and adderss in three different line.
+
+name=input("Enter your name : ")
+age=int(input("Enter your age: "))
+adderess=input("Enter your address: ")
+print(f"Name={name}\nage={age}\naddress={adderess}")
