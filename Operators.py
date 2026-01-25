27f553349b04ae188cd74cf3c5d2efee6fd4d805
Rohan-14-1 <rohanmdl2003@gmail.com>
2026-01-25 22:09:04 +0545

    Operators

---
+++ Operators.py
@@ -0,0 +1,19 @@
+# It can be divided into 6 types:
+# 1.	Arithmetic Operators
+# 2.	Comparison Operators
+# 3.	Logical Operators
+# 4.	Assignment Operators
+# 5.	Identity Operators
+# 6.	Membership Operators
+# 7.	Bitwise Operators
+
+
+#1. Arithmetic operators
+a =int(input("enter first number")) + int(input("enter second number"))      #add
+b=3-2        #subtract
+c=3*2        #multipal
+d=8/2        #divided
+e=8%3        #reminder
+f=13//3       #quotient
+g=2**2       #power
+print(f"Add: {a}\n subtract: {b}\n multipal: {c}\n divided: {d}\n reminder: {e}\n quotient: {f}\n power: {g}")
